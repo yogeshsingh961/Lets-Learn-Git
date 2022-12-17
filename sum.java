8dd4a0df08fc9af9efe428811f20f7ea9db5c622
Yogesh Singh <yogeshsingh3596@gmail.com>
2022-12-17 12:24:14 +0530

    modified sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum {
     public static void main(String[] arge){
         int a=3;
         int b=5;
-        System.out.print(a+b);
+        System.out.println("The sum is"+ a+b);
     }
 }
